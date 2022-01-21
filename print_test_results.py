18c357f744f36ebda1e26401e0542acbf076cf9f
Joshua Robison <joshuarobison@Joshuas-MacBook-Pro.local>
2022-01-20 20:49:18 -0500

    Working on test comparisson step

--- print_test_results.py
+++ print_test_results.py
@@ -1,1 +1,21 @@
-results_dir = 'test-results'
+import os
+
+results_dir = 'test_results'
+
+def main():
+    for filename in os.listdir(results_dir):
+        filepath= os.path.join(results_dir,filename)
+        if os.path.isfile(filepath):
+            print("Test results for: " + str(filepath))
+            with open(filepath, 'r') as f:
+                print(f.read())
+            print('\n\n\n')
+
+    return
+
+if __name__ == "__main__":
+   main()
+        
+        
+
+
