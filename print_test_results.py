99e66fa9462c2b4c7f3b92f9ef45649500c35e51
Joshua Robison <joshuarobison@Joshuas-MacBook-Pro.local>
2022-01-27 10:36:34 -0500

    Adding upload and deployment steps

--- print_test_results.py
+++ print_test_results.py
@@ -2,6 +2,14 @@ import os
 
 results_dir = ['/inputs/test-results-a', '/inputs/test-results-b']
 
+accuracy = 0
+# model_path = 'sample_model_path.pth'
+
+ref_dict = {'resnet18-epochs-1.txt': 'dsrsgtp083suuyu'
+            ,'resnet34-epochs-1.txt': 'dsrzkbpudtxoz5q'}
+
+model_ref = 'sample-ref'
+
 def main():
     for folder in results_dir:
         for filename in os.listdir(folder):
@@ -9,8 +17,16 @@ def main():
             if os.path.isfile(filepath):
                 print("Test results for: " + str(filepath))
                 with open(filepath, 'r') as f:
-                    print(f.read())
+                    # print(f.read())
+                    results = f.read()
+                    print(results)
+                    temp_accuracy = int(results[-3:-1])
+                    if temp_accuracy > accuracy:
+                        model_ref = ref_dict[filename]
                 print('\n\n')
+
+    with open("/outputs/bestmodel", "w") as o:
+        o.write(model_ref)
 
     return
 
